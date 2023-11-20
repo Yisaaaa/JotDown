b449fee39c719dea7b8b6e3d59b2bd42c16a1594
Yisaaaa <ryansanisit19@gmail.com>
2023-11-20 14:06:23 +0800

    minor updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Editor from "./Editor";
 import Preview from "./Preview";
