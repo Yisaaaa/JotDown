f314a514c4e226bb0e60a68e542fc89d9b743b26
Yisaaaa <ryansanisit19@gmail.com>
2023-11-26 14:26:36 +0800

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
@@ -18,3 +19,6 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
 export const notesCollection = collection(db, "notes");
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
